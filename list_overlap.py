65a1214f5b1e5682b5fe0e95528323e322ca5278
sharleenawinja <awinjasharleen@gmail.com>
2023-04-16 20:38:09 +0300

    first method

--- list_overlap.py
+++ list_overlap.py
@@ -3,3 +3,20 @@
 # b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). 
 # Make sure your program works on two lists of different sizes.
+
+def list_overlap(first_list, second_list):
+    overlapping = []
+    # looping through shortest array to avoid number of iterations
+    if len(first_list) > len(second_list):
+        for el in second_list:
+            if el in first_list:
+                overlapping.append(el)
+        return overlapping
+    else:
+        for el in first_list:
+            if el in second_list:
+                overlapping.append(el)
+        return overlapping
+
+
+print(list_overlap([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]))          
