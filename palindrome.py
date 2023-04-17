78ce0440a7fade7eea8f4aa17063f209e9a83553
sharleenawinja <awinjasharleen@gmail.com>
2023-04-17 14:23:11 +0300

    palindrome using for loops

--- palindrome.py
+++ palindrome.py
@@ -15,3 +15,17 @@ if is_palindrome(user_input):
     print("The string is a palindrome.")
 else:
     print("The string is not a palindrome.")
+
+#  using for loops
+def reverse(word):
+	x = ''
+	for i in range(len(word)):
+		x += word[len(word)-1-i]
+	return x
+
+word = input('give me a word:\n')
+x = reverse(word)
+if x == word:
+	print('This is a Palindrome')
+else:
+	print('This is NOT a Palindrome')
