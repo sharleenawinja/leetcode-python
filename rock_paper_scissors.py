71840db67d7745b66f5edf5a80bfc9c3b12552b1
sharleenawinja <awinjasharleen@gmail.com>
2023-04-21 12:35:10 +0300

    improving code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,23 +3,26 @@
 def rock_paper_scissors(choice_one, choice_two):
     while choice_one == choice_two:
         print("You tie")
-    if (choice_one == "rock"):
+    if choice_one not in ("rock", "paper", "scissors"):
+        print("%s has entered an invalid input" % user_one)
+    elif choice_two not in ("rock", "paper", "scissors"):
+        print("%s has entered an invalid input" % user_two)
+    elif (choice_one == "rock"):
         if(choice_two == "paper"):
             print("%s, won this round" % user_two)
-        else:
+        elif(choice_two == "scissors"):
             print("%s, won this round" % user_one)   
     elif(choice_one == "paper"):
         if(choice_two == "rock"):
             print("%s, wins this round" % user_one)
-        else:
+        elif(choice_two == "scissors"):
             print("%s, wins this round" % user_two)
     elif (choice_one == "scissors"):
         if(choice_two == "rock"):
             print("%s, wins this round" % user_two)
-        else:
+        elif(choice_one == "paper"):
             print("%s, wins this round" % user_one)
-    else:
-        print("Enter a valid input")
+    
         
 user_one = input("User one, what is your name: ")
 user_two = input("User two, what is your name: ")
