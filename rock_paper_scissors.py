622adb990ab022e8751c3edbe58c70d821a73d08
sharleenawinja <awinjasharleen@gmail.com>
2023-04-21 12:51:33 +0300

    user vs computer

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,6 +3,7 @@
 def rock_paper_scissors(choice_one, choice_two):
     while choice_one == choice_two:
         print("You tie")
+        break
     if choice_one not in ("rock", "paper", "scissors"):
         print("%s has entered an invalid input" % user_one)
     elif choice_two not in ("rock", "paper", "scissors"):
@@ -34,5 +35,39 @@ choice_one = input("%s, do you want to pick rock, paper or scissors: " % user_on
 choice_two = input("%s, do you want to pick rock, paper or scissors: " % user_two)
 
 rock_paper_scissors(choice_one, choice_two)
+
+# comp versus user
+import random
+
+def user_v_comp(user_choice):
+    computer_choice = random.choice(["rock", "paper", "scissors"])
+    print("You choose", user_choice)
+    print("Computer chose", computer_choice)
+    
+    if(user_choice == computer_choice):
+        print("You tie")
+        return
+    elif user_choice not in ("rock", "paper", "scissors"):
+        print("User has entered an invalid input")
+    elif (user_choice == "rock"):
+        if(computer_choice== "paper"):
+            print(" Computer won this round")
+        elif(computer_choice == "scissors"):
+            print("User won this round")   
+    elif(user_choice == "paper"):
+        if(computer_choice== "rock"):
+            print("User wins this round")
+        if(computer_choice== "scissors"):
+            print("Computer wins this round")
+    elif (user_choice == "scissors"):
+        if(computer_choice == "rock"):
+            print("Computer wins this round")
+        elif(computer_choice == "paper"):
+            print("User wins this round")
+
+user_choice = input("Choose between rock, paper, and scissors: ")
+user_v_comp(user_choice)
+    
+
 
 
