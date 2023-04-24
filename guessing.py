048580e8dc5360ca273cc423a8ef7a3aaee618f5
sharleenawinja <awinjasharleen@gmail.com>
2023-04-24 12:56:11 +0300

    second method

--- guessing.py
+++ guessing.py
@@ -33,11 +33,39 @@ def guessNumber(user_guess, random_number):
             next_guess = int(next_trial)
             guessNumber(next_guess, random_number)
 
-user_guess = int(input("Enter a number: "))
+user_guess = int(input("Enter a number between 1 and 9: "))
 random_number = random.randint(1, 9)
 guesses = 1
 print(random_number)
 guessNumber(user_guess, random_number)
+
+# import random
+
+def guess_number():
+    random_number = random.randint(1, 9)
+    num_guesses = 0
+    while True:
+        user_guess = input("Guess a number between 1 and 9 (or type 'exit' to quit): ")
+        if user_guess.lower() == "exit":
+            print(f"You made {num_guesses} guesses.")
+            print("Exiting the game...")
+            break
+        try:
+            user_guess = int(user_guess)
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 9 (or type 'exit' to quit).")
+            continue
+        num_guesses += 1
+        if user_guess == random_number:
+            print("Congratulations, you guessed the number!")
+            print(f"You made {num_guesses} guesses.")
+            break
+        elif user_guess < random_number:
+            print("Too low! Guess again.")
+        else:
+            print("Too high! Guess again.")
+
+guess_number()
 
 
 
