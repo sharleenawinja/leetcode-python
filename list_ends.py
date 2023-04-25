c1b762ae3e15134a5f6544cf90fefedebf880536
sharleenawinja <awinjasharleen@gmail.com>
2023-04-25 10:34:17 +0300

    first and last using slicing

--- list_ends.py
+++ list_ends.py
@@ -9,3 +9,10 @@ def last_and_first(num_list):
     return [first_element, last_element]
 
 print(last_and_first([5, 10, 15, 20, 25]))
+
+# finding first and last element using slicing
+def last_and_first_using_slicing(num_list):
+    return num_list[::len(num_list)-1]
+
+print(last_and_first_using_slicing([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(last_and_first_using_slicing([5, 10, 15, 20, 25]))
