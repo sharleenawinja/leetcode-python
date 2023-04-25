24c3e6b60ae51972bb4a5fb69dc63775f70809ed
sharleenawinja <awinjasharleen@gmail.com>
2023-04-25 10:04:26 +0300

    slicing

---
+++ slicing.py
@@ -0,0 +1,10 @@
+# SLICING
+# in python, a list is a collection of ordered elements
+# list slicing is a way to extract a subset of elements from a list by specifying a range of indices
+# list slicing is done through a colon operator
+# the sytax for list slicing is as follows
+# list[start:end:step]
+# start => index of the first element you want to include in the slice(inclusive)
+# end => index of the last element you want to include in the slice(exclusive)
+# step => the interval between each element in the slice (optiional, default is 1)
+
