f746ef82db941b56f68f56f2a0e362bc402cbe7e
sharleenawinja <awinjasharleen@gmail.com>
2023-04-28 15:15:02 +0300

    first method

---
+++ remove_duplicates.py
@@ -0,0 +1,10 @@
+# Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+def remove_duplicates_1(lst):
+    new_lst = []
+    for item in lst:
+        if item not in new_lst:
+            new_lst.append(item)
+    return new_lst
+
+names = ["Michele", "Robin", "Sara", "Michele"]
+print(remove_duplicates_1(names))
