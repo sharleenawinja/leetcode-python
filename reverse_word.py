e6d273a6dba41956d9c26f82f32babbc6e760884
sharleenawinja <awinjasharleen@gmail.com>
2023-04-29 21:36:36 +0300

    using list comprehension

--- reverse_word.py
+++ reverse_word.py
@@ -9,3 +9,14 @@ input_string = input("Enter a long string containing multiple words: ")
 reversed_string = reverse_words(input_string)
 print("The same string, except with the words in backwards order:")
 print(reversed_string)
+
+
+# using list comprehesion
+input_string = input("Enter a long string containing multiple words: ")
+
+words = input_string.split()
+reversed_words = [words[i] for i in range(len(words)-1, -1, -1)]
+reversed_string = " ".join(reversed_words)
+
+print("The same string, except with the words in backwards order:")
+print(reversed_string)
