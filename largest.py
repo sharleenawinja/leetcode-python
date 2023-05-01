9cb54d78f19a4b2794adeec624cc18761e87ae29
sharleenawinja <awinjasharleen@gmail.com>
2023-05-01 23:28:41 +0300

    using if ese

---
+++ largest.py
@@ -0,0 +1,10 @@
+# Implement a function that takes as input three variables, and returns the largest of the three.  five python solution
+
+# using if else
+def find_largest(a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    else:
+        return c
